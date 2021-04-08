155565b12f86246065a7c3e7d4b27417c49cbc45
Laura12345678 <>
2021-04-08 16:14:09 +0000

    l

--- Programa.java
+++ Programa.java
@@ -1,41 +1,33 @@
 import java.util.Scanner;
+import java.util.Random;
+ class Main {
 
-public class Programa {
-	
-	void iniciar(){
-		
-		int valor=generarAleatorio();
-		int num;
-		int resul;
-		
-		do{
-			num=pedirNumero();
-			resul=compararNumeros(num, valor);
-			System.out.println(resul);
-			
-		}while(resul!=0);
-		System.out.println("Felicitaciones acertaste!!");
-	}
-	
-	//devuelve un numero aleatorio entre 1 y 100
-	int generarAleatorio(){
-		//completar aquí
-	}
-	
-	//pide un nro por pantalla
-	 int pedirNumero(){
-		 Scanner entrada=new Scanner(System.in);
-		 
-		////completar aquí
-		 
-	 }
-	
-	 //compara dos enteros a y b. Devuelve un String indicando si es menor, mayor o igual
-	 int compararNumeros(int a, int b){
-		 //completar aquí
-	 }
-	
+    public static void main(String [] Args) {
+        String j = "1";
+        while (j.equals("1")) {
+
+            System.out.println ("\f");
+            System.out.println ("Adivina el número del 1 al 1000 que hemos pensado");
+
+            Random randomGenerator = new Random();
+            int randomInt = randomGenerator.nextInt(1000);
+            int i= 0;
+            while (i < 1000) {
+                Scanner entrada = new Scanner (System.in);
+                int primerIntento = entrada.nextInt();
+                if (primerIntento == randomInt) {
+                    System.out.println ("Felicitaciones, acertó el número");
+                    break;
+                } else if (primerIntento < randomInt) {
+                    System.out.println ("Demasiado chico");
+                } else {
+                    System.out.println ("Demasiado grande");
+                }
+                i++;
+            }
+
+        }
+    }
 }
 
 
-}
